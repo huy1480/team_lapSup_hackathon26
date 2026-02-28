3ba79e105b9e272be77b8e0a406a494fd7297a45
huy1480 <huy@g.clemson.edu>
2026-02-28 10:46:05 -0500

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
+read -p "API Key: " api_key
+
+echo "Checking python dependencies"
+
+if [ -x "$(command -v pacman)" ]; then
+	sudo pacman -S python-dotenv python-google-api-core
+else
+	pip install python-dotenv genai
+
+fi
+
 mkdir build
 cd build
+
+touch .env
+echo "GEMINI_API_KEY='${api_key}'" > .env
+
 cmake ..
 cmake --build .
 cp -r ../assets .
+
+mv ./assets/gemini_dialog.py .
