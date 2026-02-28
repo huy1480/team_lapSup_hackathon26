26cf3b7ba780043436d81e9fa99db27582086c5b
huy1480 <huy@g.clemson.edu>
2026-02-28 13:57:00 -0500

    update .bat file

--- build.bat
+++ build.bat
@@ -1,8 +1,29 @@
 @echo off
+set /p api_key="API Key: "
 
-pip install genai python-dotenv
-mkdir build
+echo Checking python dependencies...
+
+:: Batch doesn't have a direct equivalent to pacman, 
+:: so we default to pip for Windows environments.
+pip install python-dotenv google-generativeai
+
+if not exist "build" (
+    mkdir build
+)
 cd build
+
+:: Create the .env file and write the key
+echo GEMINI_API_KEY='%api_key%' > .env
+
+:: Run CMake build process
 cmake ..
 cmake --build .
-xcopy /E /I ..\assets .\assets
+
+:: Copy assets folder (using xcopy for directories)
+xcopy /E /I /Y "..\assets" ".\assets"
+
+:: Move the specific python file
+move ".\assets\gemini_dialog.py" "."
+
+echo Setup complete.
+pause
